bc592c303b1525f2c640e6bc9cb4adedba66067f
Jesse Bellingham <5078290+jbellingham@users.noreply.github.com>
2024-04-01 20:26:32 +1100

    Core settings in separate module

--- nix-darwin-config/flake.nix
+++ nix-darwin-config/flake.nix
@@ -6,11 +6,18 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{
+    self,
+    nixpkgs,
+    darwin,
+    ...
+  }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
@@ -20,13 +27,6 @@
       nixpkgs.config.allowUnfree = true;
       # enables installing applications that don't list darwin as a supported OS
       nixpkgs.config.allowUnsupportedSystem = true;
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
 
       # Create /etc/zshrc that loads the nix-darwin environment.
       programs.zsh.enable = true;  # default shell on catalina
@@ -49,9 +49,10 @@
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#Jesses-MacBook-Pro
-    darwinConfigurations."Jesses-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+    darwinConfigurations."Jesses-MacBook-Pro" = darwin.lib.darwinSystem {
       modules = [
         configuration
+        ./modules/nix-core.nix
         ./modules/apps.nix
         ];
     };
