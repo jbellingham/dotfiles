66ce2bc11deb388dab430a8639535739b0f34147
Jesse Bellingham <5078290+jbellingham@users.noreply.github.com>
2025-07-08 14:47:42 +1000

    Added a bunch of nix and brew managed apps

--- nix-darwin-config/modules/apps.nix
+++ nix-darwin-config/modules/apps.nix
@@ -6,22 +6,40 @@ environment.systemPackages = with pkgs;
         # ---
         # cli tools
         # ---
+        awscli2
         bat
-        # direnv
+        darwin.trash
+        fzf
+        git
         jump
+        jq
+        lua
         oh-my-posh
+        shellcheck
         tldr
         zsh-fzf-tab
-        # vim
 
+        # fonts
+        hack-font
+        meslo-lg
+        meslo-lgs-nf
+        source-code-pro
     ];
 
     homebrew = {
         enable = true;
-        brews = [
-            "trash"
-            "direnv"
+        taps = [
+            "homebrew/cask-fonts"
         ];
+        brews = [
+            "direnv"
+            "nowplaying-cli"
+            "switchaudio-osx"
+        ];
+        masApps = {
+            "1Password for Safari" = 1569813296;
+            # "Xcode" = 497799835;
+        };
         casks = [
             # ---
             # GUI Apps
@@ -29,6 +47,7 @@ environment.systemPackages = with pkgs;
             "1password"
             "brave-browser"
             "cheatsheet"
+            "dbeaver-community"
             "discord"
             "docker"
             "flux"
@@ -43,6 +62,15 @@ environment.systemPackages = with pkgs;
             "stats"
             "visual-studio-code"
             "wezterm"
+
+            # ---
+            # Fonts
+            # ---
+            "font-hack-nerd-font"
+            "font-jetbrains-mono-nerd-font"
+            "font-sf-mono"
+            "font-sf-pro"
+            "sf-symbols"
         ];
     };
 }
