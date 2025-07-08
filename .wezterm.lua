c46a811bddc67fb723a1382144c7d4a1f053963d
Jesse Bellingham <5078290+jbellingham@users.noreply.github.com>
2025-07-08 14:42:21 +1000

    Lazygit config

---
+++ .wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'AdventureTime'
+
+config.font_size = 16.0
+config.color_scheme = 'Afterglow'
+config.window_background_opacity = 0.9
+config.window_background_image = '/Users/jessebellingham/terminal-background.jpg'
+config.window_background_image_hsb = {
+    brightness = 0.2,
+}
+
+-- and finally, return the configuration to wezterm
+return config
