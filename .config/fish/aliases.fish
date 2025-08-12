9e609fd65d9ea5330428c1a708f713ca50584be7
Jesse Bellingham <5078290+jbellingham@users.noreply.github.com>
2025-08-12 14:57:05 +1000

    Fish aliases and ghosty and lazygit config

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -7,17 +7,17 @@ function ..... ; cd ../../../.. ; end
 abbr mv 'mv -v'
 abbr rm 'rm -v'
 abbr cp 'cp -v'
+abbr omd 'overmind'
 
 alias ls="ls --color=auto $argv"
 alias ll="ls -la --color=auto $argv"
 
 # remove file from quarantine
 alias unquarantine="sudo xattr -rd com.apple.quarantine"
-
+alias be="bundle exec"
 alias cat="bat"
 alias top="btop"
 alias bu="brew upgrade"
-alias be="bundle exec"
 alias hgrep="history 1 | grep"
 alias grep="rg -iF --color=auto"
 alias formatjson="pbpaste | jq . | pbcopy"
